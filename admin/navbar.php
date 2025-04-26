b29f4d9bebeb3932c4243974fc84850415343518
Ahmad Ghosen <ahmadghosen20@gmail.com>
2025-04-26 23:29:46 +0300

    edit the nav bar

--- admin/navbar.php
+++ admin/navbar.php
@@ -11,8 +11,18 @@
     <i class="fas fa-box"></i><span>Packages</span>
   </div>
   <div class="nav-item" onclick="window.location.href='booking.php'">
-    <i class="fas fa-users"></i><span>Booking</span>
-  </div>
+  <i class="fas fa-ticket-alt"></i><span>Booking</span>
+</div>
+<div class="nav-item" onclick="window.location.href='admin.php'">
+  <i class="fas fa-user-shield"></i><span>Admin Management</span>
+</div>
+<div class="nav-item" onclick="window.location.href='user.php'">
+  <i class="fas fa-user"></i><span>User Management</span>
+</div>
+<div class="nav-item" onclick="window.location.href='operator.php'">
+  <i class="fas fa-user-tie"></i><span>Operator Management</span>
+</div>
+
 
   <div class="nav-item" onclick="window.location.href='../logout.php'">
     <i class="fas fa-door-open"></i><span>Logout</span>
